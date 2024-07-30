d4c3e61e1f66b3e18248a3cb8efa3781382cb312
YoussefYaserjr <133673347+YoussefYaserjr@users.noreply.github.com>
2024-07-30 18:00:24 +0300

    js

---
+++ main.js
@@ -0,0 +1,18 @@
+
+const a=prompt(" Even||Odd: ");
+
+if(a&1)console.log("number is Odd");
+else console.log("number is Even");
+
+const r=prompt("enter raduis of circul: ");
+const pi=3.141592653589793238462643383279502884197;
+console.log(pi*r*r);
+console.log(2*pi*r);
+const h=prompt("positive||negative: ");
+if(h>0)console.log("number is postive");
+else console.log("number is negative");
+
+
+let x=prompt("1-sum two numbers: ");
+let y=prompt("2-sum two numbers: ");
+console.log(+x+ +y);
